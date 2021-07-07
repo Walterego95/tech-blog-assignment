a761017012e7adefb2ab02488c397426c930ce04
walterego <walter.scimone@gmail.com>
2021-07-07 19:03:54 -0400

    utils created, started withAuth

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+//function to see if user is authenticated... 
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
